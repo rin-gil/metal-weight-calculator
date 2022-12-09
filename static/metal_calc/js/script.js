4d6af2fab86c97afd568ea97ef146e439a6990d8
rin-gil <91410075+rin-gil@users.noreply.github.com>
2022-12-09 15:01:20 +0200

    refactor: Change paths to static folder, templates folder
    Signed-off-by: rin-gil <91410075+rin-gil@users.noreply.github.com>

---
+++ static/metal_calc/js/script.js
@@ -0,0 +1,32 @@
+/**
+ * When selecting a metal type, switches the visibility of the items in the list of alloys, according to the selected metal type.
+ *
+ * @return void
+ * @param selectedMetal selected type of metal
+ */
+function selectMetal(selectedMetal) {
+    let i, options;
+    options = document.getElementsByClassName("grade");
+    for (i = 0; i < options.length; i++) {
+        if (options[i].className.includes(selectedMetal)) {
+            options[i].className = options[i].className.replace("hidden", "visible")
+        } else {
+            options[i].className = options[i].className.replace("visible", "hidden")
+            options[i].removeAttribute("selected")
+        }
+    }
+    document.getElementById("metal_alloy").value = 0
+    // When selecting a metal, it also changes the value sent to the form with id="calculator"
+    document.getElementById("metal_alloy_selected").value = 0
+    document.getElementById("metal_selected").value = selectedMetal
+}
+
+/**
+ * When selecting a metal alloy, it also changes the value sent to the form with id="calculator"
+ *
+ * @return void
+ * @param selectedAlloy selected alloy of metal
+ */
+function selectMetalAlloy(selectedAlloy) {
+    document.getElementById("metal_alloy_selected").value = selectedAlloy
+}
